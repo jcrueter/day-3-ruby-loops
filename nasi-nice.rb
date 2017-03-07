8c8b920b3c40d0ce6533061aa9eabd90f93456b0
jcrueter <michiel@rueter.nl>
2017-03-07 16:35:25 +0100

    Iterate through each step

--- nasi-nice.rb
+++ nasi-nice.rb
@@ -8,9 +8,9 @@ def print_progress_bar
   puts "\n"
 end
 
-QUANTITY_RICE = "TONS"
+QUANTITY_RICE = 10
 AMOUNT_VEGETABLES = "ALLOT"
-NUM_CHICKENS = 2
+NUM_CHICKENS = 3
 
 ingredients = [
   { name: 'rice', quantity: QUANTITY_RICE },
@@ -38,3 +38,66 @@ loop do
   else puts "That is not a valid input, so try again."
   end
 end
+steps = [
+  { description: "Cook rice", action: "cook_rice" },
+  { description: "Fry chickens", action: "fry_chickens" },
+  { description: "Chop vegetables", action: "chop_veggies" },
+  { description: "Prepare bumbu", action: "prep_bumbu" },
+  { description: "Throw it all together", action: "throw_together" },
+  { description: "Add a jar of sambal", action: "add_sambal" },
+]
+print_divider
+puts "Below you can see the steps to follow:"
+steps.each_with_index do |step, index|
+  puts (index + 1).to_s + ") " + step[:description]
+end
+def generic_recipe_step
+  puts "On it!"
+  print_progress_bar
+end
+
+def cook_rice
+  counter = 0
+  while counter < QUANTITY_RICE
+    counter += 1
+    print "Cook rice grain #{counter}"
+    print_progress_bar
+  end
+end
+
+def peel_chickens
+  counter = 0
+  while counter < NUM_CHICKENS
+    counter += 1
+    print "Peel chicken #{counter}"
+    print_progress_bar
+  end
+end
+
+def add_chickens_to_pan
+  for counter in 1..NUM_CHICKENS
+    print "Add chicken #{counter} to the pan."
+    print_progress_bar
+  end
+end
+
+def ask_if_ready(step, index)
+  puts "Are you ready for step #{index + 1}?\n(#{step[:description]})"
+  answer = gets.chomp
+
+  answer.upcase == 'Y'
+end
+
+steps.each_with_index do |step, index|
+  print_divider
+
+  loop do
+    ready = ask_if_ready
+    break if ready
+
+    puts "OK, I will give you some extra time..."
+    print_progress_bar
+  end
+  send(step[:action])
+end
+  
